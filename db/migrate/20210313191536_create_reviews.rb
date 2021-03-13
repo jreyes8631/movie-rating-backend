9371e6e2a94af42f0fea38a7bdba0e5813f90cec
jreyes8631 <jreyes8631@gmail.com>
2021-03-13 14:17:53 -0500

    add review scaffold

---
+++ db/migrate/20210313191536_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+      t.string :comment
+      t.references :user, null: false, foreign_key: true
+      t.references :movie, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

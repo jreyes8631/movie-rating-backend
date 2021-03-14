40bcf5308c97bb9943e321410d13343e3379f179
jreyes8631 <jreyes8631@gmail.com>
2021-03-14 10:19:22 -0400

    add joing model to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :movies
-  has_many :reviews
+  has_many :reviews, through: :movies
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end

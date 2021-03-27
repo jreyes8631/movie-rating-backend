038aa0711cfd51574cd91ea25035bde3cb7b5320
jreyes8631 <jreyes8631@gmail.com>
2021-03-27 13:20:46 -0400

    add movie validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
     belongs_to :user
     has_many :reviews
+    validates :genre, :classification, :duration, :release_date, :director, :description, presence: true
+    validates :title, presence: true, uniqueness: true
 end

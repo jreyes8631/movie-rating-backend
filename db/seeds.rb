8e3868dc5ec3b7720108fbf5160fe0e8f09f0c9d
jreyes8631 <jreyes8631@gmail.com>
2021-04-11 11:21:34 -0400

    refactor seed to show movies for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ new_user = User.create(email:"test@test.com", password:"password1")
 
 user = User.first
 
-@movie1 = user.movies.find_or_create_by(title: 'Godzilla', genre: 'Action', description: 'Monster movie', classification: 'PG-13', duration: '123 min', release_date: '2014', director: 'Gareth Edwards')
-@movie2 = user.movies.find_or_create_by(title: 'Godzilla 2', genre: 'Action', description: 'Monster movie', classification: 'PG-13', duration: '123 min', release_date: 7/12/215, director: 'Gareth Edwards')
-
+movie1 = user.movies.find_or_create_by(title: 'Godzilla vs. Kong', genre: 'Action, Sci-Fi, Thriller', description: 'The epic next chapter in the cinematic Monsterverse pits two of the greatest icons in motion picture history against one another - the fearsome Godzilla and the mighty Kong - with humanity caught in the balance.', classification: 'PG-13', duration: '123 min', release_date: '31 March 2021', director: 'Adam Wingard')
+movie2 = user.movies.find_or_create_by(title: 'Wonder Woman 1984', genre: 'Action, Adventure, Fantasy', description: 'Diana must contend with a work colleague and businessman, whose desire for extreme wealth sends the world down a path of destruction, after an ancient artifact that grants wishes goes missing.', classification: 'PG-13', duration: '2h 31min', release_date:' 25 December 2020', director: 'Patty Jenkins')
+movie3 = user.movies.find_or_create_by(title: 'Outside the Wire', genre: 'Action, Adventure, Fantasy', description: 'In the near future, a drone pilot sent into a war zone finds himself paired with a top-secret android officer on a mission to stop a nuclear attack.', classification: 'R', duration: '1h 54min', release_date:'15 January 2021', director: 'Patty Jenkins')

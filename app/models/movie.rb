21c83da3b0932d0741b7f1411e0e9ead9ce7a0f5
jreyes8631 <jreyes8631@gmail.com>
2021-04-11 10:09:33 -0400

    add dependent destroy to movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
     belongs_to :user
-    has_many :reviews
+    has_many :reviews, dependent: :destroy
     validates :genre, :classification, :duration, :release_date, :director, :description, presence: true
     validates :title, presence: true, uniqueness: true
 end

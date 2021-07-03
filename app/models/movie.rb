0125e2f27f296900b5b0543cda1df6314d228a66
jreyes8631 <jreyes8631@gmail.com>
2021-07-03 11:39:53 -0400

    add poster to movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
     belongs_to :user
+    has_one_attached: :poster
     has_many :reviews, dependent: :destroy
     validates :genre, :classification, :duration, :release_date, :director, :description, presence: true
     validates :title, presence: true, uniqueness: true

bf6b4cde804be6dea6a4aae177ea5015393caf1a
jreyes8631 <jreyes8631@gmail.com>
2021-07-03 13:50:52 -0400

    add poster to movie serializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,6 @@
 class MovieSerializer
   include JSONAPI::Serializer
-  attributes :id, :title, :genre, :classification, :duration, :release_date, :director, :description
+  attributes :id, :title, :genre, :classification, :duration, :release_date, :director, :description, :poster
   has_many :reviews
+  has_one: :poster
 end

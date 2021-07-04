339be8a3f1503e12cb39482d33b04a58632ecab4
jreyes8631 <jreyes8631@gmail.com>
2021-07-04 19:04:53 -0400

    define poster url method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,11 @@
 class Movie < ApplicationRecord
     belongs_to :user
-    has_one_attached: :poster
+    has_one_attached :poster
     has_many :reviews, dependent: :destroy
     validates :genre, :classification, :duration, :release_date, :director, :description, presence: true
     validates :title, presence: true, uniqueness: true
+
+    def poster_url
+        Rails.application.routes.url_helpers.url_for(poster) if poster.attached?
+    end
 end
